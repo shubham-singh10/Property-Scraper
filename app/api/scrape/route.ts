1d1d8367316694a85d2649fc625fec737f2c8120
Shubham Kumar Singh <shubhamsinghh9599@gmail.com>
2024-11-16 15:39:19 +0530

    vercel.json added

--- app/api/scrape/route.ts
+++ app/api/scrape/route.ts
@@ -26,7 +26,7 @@ export async function POST(req: Request) {
 
         const page = await browser.newPage();
 
-        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+        await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
         await page.setExtraHTTPHeaders({
             'Accept-Language': 'en-US,en;q=0.9',
             'Accept-Encoding': 'gzip, deflate, br',
